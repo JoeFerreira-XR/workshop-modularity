93624a974c1ef387361ceca98471f544be5f4e94
Joe Ferreira <joelson.bferreira@pucpr.br>
2025-11-05 15:04:52 -0300

    Refactored EntityBrain to extend AEntityComponent

--- Assets/_scripts/1 Abstract Classes and Interfaces/Entity/EntityComponents/EntityBrain.cs
+++ Assets/_scripts/1 Abstract Classes and Interfaces/Entity/EntityComponents/EntityBrain.cs
@@ -2,32 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityBrain : MonoBehaviour, IEntityComponent
+public class EntityBrain : AEntityComponent
 {
-    public Entity Entity { get; private set; }
-    public bool IsComponentActive { get; private set; }
 
-    public void OnEntityRegistered(Entity entity)
-    {
-        Entity = entity;
-
-        Debug.Log($"Entity {entity.name} registered with brain {name}");
-    }
-
-    public void ResolveDependencies()
-    {
-        Debug.Log($"Resolving dependencies for brain {name}");
-    }
-
-    public void ActivateComponent()
-    {
-        IsComponentActive = true;
-        Debug.Log($"Activated brain {name}");
-    }
-
-    public void DeactivateComponent()
-    {
-        IsComponentActive = false;
-        Debug.Log($"Deactivated brain {name}");
-    }
 }
