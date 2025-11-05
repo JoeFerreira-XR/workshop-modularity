afd3cbf783b389697cbba7821c371e2ec13cf933
chibo <projectchibo@gmail.com>
2025-11-05 11:44:12 -0300

    Simple movement implementation

--- Assets/_scripts/1 Abstract Classes and Interfaces/PlayerBehaviour.cs
+++ Assets/_scripts/1 Abstract Classes and Interfaces/PlayerBehaviour.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float speed;
+    Vector2 MovementDirection
     {
-        
+        get => new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
     void Update()
     {
-        
+        Move(MovementDirection, speed);
+    }
+
+    /// <summary>
+    /// Move the player in a direction
+    /// </summary>
+    /// <param name="direction">x = horizontal, y = vertical</param>
+    /// <param name="speed">speed of the player</param>
+    void Move(Vector2 direction, float speed)
+    {
+        Vector3 direction3 = new(direction.x, 0, direction.y);
+        transform.Translate(speed * Time.deltaTime * direction3);
     }
 }
